9ea57cf05e74b42108e49ba08b5efbde9b8df9b1
Derrik Nasca <derriknasca@gmail.com>
2023-07-11 16:22:11 -0700

    Added new API endpoints   prompt   prompt\[id]   prompt\new   users\[id]\posts   auth\[...nextauth]     googleauth
    Added components
      Feed
      Form
      Nav
      Profile
      PromptCard
      Provider
    
    Added models
      prompt
      user
    
    Added tailwind.config.js
    Added globals.css
    
    Added top level app\
      layout.jsx
      page.jsx
    
    Added pages
      profile, create-prompt, update-prompt
    
    Added public\assets\icons
    Added public\assets\images
    
    Added utils
      database.js

---
+++ app/update-prompt/page.jsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+import Form from "@components/Form";
+
+const UpdatePrompt = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const promptId = searchParams.get("id");
+
+  const [post, setPost] = useState({ prompt: "", tag: "", });
+  const [submitting, setIsSubmitting] = useState(false);
+
+  useEffect(() => {
+    const getPromptDetails = async () => {
+      try {
+        const response = await fetch(`/api/prompt/${promptId}`);
+        const data = await response.json();
+  
+        if (response.ok) {
+          setPost({
+            prompt: data.prompt,
+            tag: data.tag,
+          });
+        } else {
+          // Handle error case, e.g., show an error message
+          console.error("Error fetching prompt details");
+        }
+      } catch (error) {
+        // Handle network or parsing error
+        console.error("Error fetching prompt details", error);
+      }
+    };
+  
+    if (promptId) {
+      getPromptDetails();
+    }
+  }, [promptId]);
+
+  const updatePrompt = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    if (!promptId) return alert("Missing PromptId");
+
+    try {
+      const response = await fetch(`/api/prompt/${promptId}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <Form
+      type='Edit'
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={updatePrompt}
+    />
+  );
+};
+
+export default UpdatePrompt;
